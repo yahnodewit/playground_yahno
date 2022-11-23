5ca25d7693a31cf43c935709ca715bbd181330f0
yahnodewit <92160439+yahnodewit@users.noreply.github.com>
2022-11-23 12:07:31 +0100

    first try react

---
+++ first_try_react/react-essentials/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Header</h1>
+      <h2>Main</h2>
+      <h3>Footer</h3>
+    </div>
+  );
+}
+
+export default App;
