b8f2838dbaf81e08521500b9632514230af880a6
yahnodewit <92160439+yahnodewit@users.noreply.github.com>
2022-11-23 12:07:07 +0100

    animated logo files

---
+++ animated_logo/js/animation.js
@@ -0,0 +1,7 @@
+var animation = bodymovin.loadAnimation({
+    container: document.getElementById('animation'),
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: './js/animation.json'
+  })
